d4265fc255d19088b23a905b7d732f5b3758031b
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-02-26 01:11:16 +0100

    Finished up the contact card

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.cyan.shade700,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage("images/me.png"),
+                radius: 60.0,
+              ),
+              Text(
+                "Oladayo Olufemi",
+                style: TextStyle(
+                    fontSize: 30.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico'),
+              ),
+              SizedBox(
+                height: 6.0,
+              ),
+              Text(
+                "APP DEVELOPER",
+                style: TextStyle(
+                    letterSpacing: 5.0,
+                    fontFamily: 'SourceCodePro',
+                    color: Colors.white),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 20.0,
+                    color: Colors.cyan.shade900,
+                  ),
+                  title: Text(
+                    "+49 177 8416 737",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.cyan.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    size: 20.0,
+                    color: Colors.cyan.shade900,
+                  ),
+                  title: Text(
+                    "godsurest@gmail.com",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.cyan.shade900,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
+  }
+}
